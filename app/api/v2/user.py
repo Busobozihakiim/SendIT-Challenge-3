17456525cae25aa0a7e8fb10073e3e7a625ff29b
Busobozihakiim <Busobozihakiim@yahoo.com>
2018-11-21 21:39:10 +0300

    removes else condition

--- app/api/v2/user.py
+++ app/api/v2/user.py
@@ -9,12 +9,9 @@ def registers_user():
     returned = request.get_json() or {}
     new_user = UserCredentials()
 
-    #check for an empty post
     if not returned:
         return jsonify({"message":"You entered nothing", "status":"failure"})
 
-    #check for empty values in post and return missing field
-    #check for wrong input format
     for key, value in returned.items():
         if value == "":
             return jsonify({"message":"You've entered an empty value ",
@@ -25,16 +22,13 @@ def registers_user():
                             "status":"failure",
                             "Error":"{}".format(key)})
 
-    #check for existing email
     already_exits = new_user.login_user(returned['email'])
     if already_exits:
         return jsonify({"message":"Email already exists",
                         "status":"failure",
                         "data":"{}".format(returned['email'])})
-    else:
-        #create an account
-        new_user.register_user(returned['email'], returned['password'])
-        return jsonify({"message":"account created succesfully", "status":"success"}), 201
+    new_user.register_user(returned['email'], returned['password'])
+    return jsonify({"message":"account created succesfully", "status":"success"}), 201
 
 @apiv2.route('auth/login', methods=['POST'])
 def login_users():
@@ -43,12 +37,9 @@ def login_users():
     returned = request.get_json() or {}
     user = UserCredentials()
 
-    #check for an empty post
     if not returned:
         return jsonify({"message":"You entered nothing", "status":"failure"})
 
-    #check for empty values in post and return missing field
-    #check for wrong input format
     for key, value in returned.items():
         if value == "":
             return jsonify({"message":"You've entered an empty value ",
